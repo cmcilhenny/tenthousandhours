96f1bc09968493a44b55b69cad6bb894dfb52d33
courtney mcilhenny <github@courtneymcilhenny>
2014-11-01 12:53:27 -0700

    updating time output to handle hours, mins, secs < 10.

--- application.js
+++ application.js
@@ -1,23 +1,48 @@
 $( document ).ready(function() {
   // define timer variable that tracks by seconds
-  var seconds = 0;
+  var timeCount = 0;
   var interval;
   var buttonToggle = false;
 
   // make clock tick 
   function timer() {
-    seconds++;
+    timeCount++;
     // convert to time HH-MM-SS
-    var hours = Math.floor(seconds / 3600);
-    var minutes = seconds % 3600;
-    minutes = Math.floor(seconds / 60);
-    seconds = seconds % 60;
-    var time = hours + ":" + minutes + ":" + seconds;
+    var hours = Math.floor(timeCount / 3600);
+    var minutes = timeCount % 3600;
+    minutes = Math.floor(minutes / 60);
+    var seconds = timeCount % 60;
+    var time = "";
+    if (hours === 0) {
+      time += "";
+    } else if (hours< 10 && hours > 0) {
+      time += "0" + hours.toString() + ":";
+    } else if (hours >= 1000) {
+      hours.toString();
+      
+    } else {  
+      time += hours.toString() + ":";
+    };
 
+    if (minutes< 10) {
+      time += "0" + minutes.toString() + ":";
+    } else {
+      time += minutes.toString() + ":";
+    };
+
+    if (seconds < 10) {
+      time += "0" + seconds.toString();
+    } else {
+      time += seconds.toString();
+    }
+
+
+   
     // update timer on DOM
     $("#codeClock").html(time);
   };
 
+  // function timerDisplay ()
 
   //toggle on/off button function 
   $('.btn-toggle').click(function() {
